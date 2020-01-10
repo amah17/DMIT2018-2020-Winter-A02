83b3f5197f47afdf8f6e9055589f5dc2c88848a3
amah17 <46571676+amah17@users.noreply.github.com>
2020-01-10 15:50:50 -0700

    Added Album and Artist to the Entities

---
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+    {
+
+    [Table("Albums")]
+    public class Album
+    {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage = "Album Title required")]
+        [StringLength(160, ErrorMessage ="Album Title limited to 160 characters")]
+        public string AlbumTitle { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage ="Artist ID out of range")]
+        public int ArtistId { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        [StringLength(50, ErrorMessage ="Release Label is limited to 50 characters")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
+    }
+}
---
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Data.Entities
+{
+    [Table("Artist")]
+    public class Artist
+    {
+        private string _Name;
+        // If using the same names as table, then the properties can be in any order.
+        [Key]
+        public int ArtistId { get; set; }
+
+        // Fully implemented property. MUST DO FOR STRINGS. CLASS STANDARD
+        // Check if the SQL entity attribute has any constraints.
+        [StringLength(120, ErrorMessage ="Artist Name is limited to 120 characters.")]
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                //if (string.IsNullOrEmpty(value))
+                //{
+                //    _Name = null;
+                //}
+                //else
+                //{
+                //    _Name = value;
+                //}
+                _Name = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
+    }
+}
