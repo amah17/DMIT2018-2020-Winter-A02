bdabd321ae1ac966873a5e30f870a38fd9686573
amah17 <46571676+amah17@users.noreply.github.com>
2020-01-10 16:42:27 -0700

    Added two basic lookups for AlbumController and ArtistController

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Add namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class AlbumController
+    {
+        //basic query: complete list of DbSet
+        public List<Album> Album_List()
+        {
+            //setup the code block to ensure the release of the sql connection
+            using (var context = new ChinookContext())
+            {
+                // .ToList<t> is used to convert the DbSet<t> into a List<t> collection
+                return context.Albums.ToList();
+            }
+        }
+
+        //basic query: return a recorded based on pkey
+        public Album Album_FindByID(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+    }
+}
---
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Add Namespaces
+using System.Collections.Generic;
+using ChinookSystem.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    //ask yourself, what is going to be using this. This case, its another project. The WEBAPP
+    public class ArtistController
+    {
+        //basic query: complete list of DbSet
+        public List<Artist> Artist_List()
+        {
+            //setup the code block to ensure the release of the sql connection
+            using (var context = new ChinookContext())
+            {
+                // .ToList<t> is used to convert the DbSet<t> into a List<t> collection
+                return context.Artists.ToList();
+            }
+        }
+
+        //basic query: return a recorded based on pkey
+        public Artist Artist_FindByID(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+    }
+}
