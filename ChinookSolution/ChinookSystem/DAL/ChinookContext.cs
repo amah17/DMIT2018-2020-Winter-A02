f87cbf1a487fd2c7b4655af2ed13abe9189f09af
amah17 <46571676+amah17@users.noreply.github.com>
2020-01-10 16:29:39 -0700

    Review of Context class

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Add Namespaces
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+namespace ChinookSystem.DAL
+{
+    //restrict access to the database interface to within the project
+    //inherit EntityFramework interface DbContext
+    internal class ChinookContext:DbContext
+    {
+        //Constructer will pass the connection string name to the database to EntityFramework via DbContext
+        public ChinookContext() : base("ChinookDB")
+        {
+        }
+        //Create a DbSet<T> for each Entity
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
